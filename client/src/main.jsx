a493e2ec6771f9b5629cee855318b4dca33bd8f3
duongbuibd <duongbuibd@gmail.com>
2023-11-09 14:13:43 +0700

    test api

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import axios from "./utils/customFetch.js";
+
+const response = await axios.get("/test");
+console.log(response);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
