f4f04dc6da28cc4d9cab6e861c1ae5eca4216e6b
duongbuibd <duongbuibd@gmail.com>
2023-11-10 15:26:46 +0700

    login

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,6 +1,22 @@
-import { Link, Form } from "react-router-dom";
+import { Link, Form, redirect } from "react-router-dom";
 import Wrapper from "../assets/wrappers/RegisterAndLoginPage";
 import { FormRow, Logo, SubmitBtn } from "../components";
+import { toast } from "react-toastify";
+import customFetch from "../utils/customFetch";
+
+export const action = async ({ request }) => {
+  const formData = await request.formData();
+  const data = Object.fromEntries(formData);
+
+  try {
+    await customFetch.post("/auth/login", data);
+    toast.success("Login successfully.");
+    return redirect("/dashboard");
+  } catch (error) {
+    toast.error(error?.response?.data?.msg);
+    return error;
+  }
+};
 
 const Login = () => {
   return (
