6a4925dcf20ed5168a2aaf40e4336b65392cc15e
duongbuibd <duongbuibd@gmail.com>
2023-11-19 03:21:19 +0700

    controlled search

--- client/src/components/SearchContainer.jsx
+++ client/src/components/SearchContainer.jsx
@@ -3,11 +3,11 @@ import Wrapper from "../assets/wrappers/DashboardFormPage";
 import { Form, useSubmit, Link } from "react-router-dom";
 import { JOB_TYPE, JOB_STATUS, JOB_SORT_BY } from "../utils/constants";
 import { useAllJobsContext } from "../pages/AllJobs";
-import { toast } from "react-toastify";
-import customFetch from "../utils/customFetch";
 
 const SearchContainer = () => {
   const { data } = useAllJobsContext();
+  const submit = useSubmit();
+
   return (
     <Wrapper>
       <Form className="form">
@@ -20,30 +20,41 @@ const SearchContainer = () => {
             name="search"
             defaultValue=""
             placeholder="Searching..."
+            onChange={(e) => {
+              submit(e.currentTarget.form);
+            }}
           />
           <FormRowSelect
             labelText="job status"
             name="jobStatus"
             list={["all", ...Object.values(JOB_STATUS)]}
             defaultValue="all"
+            onChange={(e) => {
+              submit(e.currentTarget.form);
+            }}
           />
           <FormRowSelect
             labelText="job type"
             name="jobType"
             list={["all", ...Object.values(JOB_TYPE)]}
             defaultValue="all"
+            onChange={(e) => {
+              submit(e.currentTarget.form);
+            }}
           />
           <FormRowSelect
             name="sort"
             defaultValue="newest"
             list={[...Object.values(JOB_SORT_BY)]}
+            onChange={(e) => {
+              console.log(e.currentTarget.form);
+              submit(e.currentTarget.form);
+            }}
           />
 
           <Link to="/dashboard/all-jobs" className="btn form-btn delete-btn">
             Reset Search Values
           </Link>
-          {/* TEMP!!!! */}
-          <SubmitBtn formBtn />
         </div>
       </Form>
     </Wrapper>
